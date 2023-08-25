b7eee68f98da397e5484d86dca169fd41bb3b516
Ararat <melkumyan2077@gmail.com>
2023-08-25 23:54:03 +0300

    update authComponent

--- src/app/modules/auth/auth.component.ts
+++ src/app/modules/auth/auth.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.scss']
 })
-export class AuthComponent {
+export class AuthComponent implements OnInit{
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    if (this.authService.isLoggedIn) {
+      this.router.navigateByUrl('');
+    }
+  }
 
 }
